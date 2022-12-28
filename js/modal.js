2e24c657c43ba37606d1a124bdbe2a71f6c8fb50
marianorigo18 <marianowagnerrigo@gmail.com>
2022-12-28 15:08:30 -0300

    first commit

---
+++ js/modal.js
@@ -0,0 +1,4 @@
+//listen for click event on modal-btn and close-btn
+//when user click on modal-btn add .open-modal to modal-overlay 
+//when user click close-btn remove .open-modal to modal-overlay.
+
